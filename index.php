8eb52d0f889097c7bf2aeaefbabb57beceb99b44
Erick Romero <zyst@gmx.com>
2014-08-06 13:59:28 -0500

    Optimization

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
         
         <link rel="icon" href="images/4chon.ico">
-        <script src="js/togglerandomBG.js"></script>
         <link rel='stylesheet' href='css/style.css' type='text/css'>
+        <script src="js/togglerandomBG.js"></script>
         <title>Love</title>
     </head>
     <body onload="randomBG();">
