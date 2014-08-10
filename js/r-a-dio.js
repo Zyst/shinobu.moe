0ae61dcc60e3e9403b685e58b0250c4555a245ca
Erick Romero <zyst@gmx.com>
2014-08-10 06:11:00 -0500

    Testing DJ now, gonna do one at a time do not get greedy.

--- js/r-a-dio.js
+++ js/r-a-dio.js
@@ -1,16 +1,22 @@
 function loadAPI() {
-    
+    // This gets the PHP Code
     var getPHPString = document.getElementById("hidePHP");
     var radioAPIString = getPHPString.textContent;
 
-    // This is gonna go the way of the dodo soon. Leaving it for testing.
-    // alert(radioAPIString);
-
+    // Regex, like honestly if anyone says they understand Regexes
+    // they are filthy lying whores
     var nowPlaying = radioAPIString.match("\"np\":\".*\"?,\"listeners\"");
-    // alert(nowPlaying);
+    
+    // Updates "Now Playing"
     nowPlaying = String(nowPlaying);
     nowPlaying = nowPlaying.replace("\"np\":\"", "");
     nowPlaying = nowPlaying.replace("\",\"listeners\"", "");
+
+    // Updates "DJ"
+    var DJ = radioAPIString.match("\"djname\":\".*\"?,\"djtext\"");
+    DJ = String(DJ);
+    DJ = DJ.replace("\"djname\":\"", "");
+    DJ = DJ.replace("\",\"djtext\"", "");
 
     document.getElementById("nowPlaying").innerHTML = nowPlaying;
 }
