9e9a77f0374c9c799190b4768fff4b8a94fd649b
Erick Romero <zyst@gmx.com>
2014-08-10 04:51:28 -0500

    More JS Testing

--- js/r-a-dio.js
+++ js/r-a-dio.js
@@ -1,14 +1,18 @@
 function loadAPI() {
 
     // Goes to r-a-d.io/api, gets content
-    // var radioAPIString = "<?php Print($curlResult); ?>";
+    var radioAPIString = "<?php echo json_encode($curlResult); ?>";
 
     // The Div that contains "Now Playing"
     // Do I need this?
     // var nowPlayingDiv = document.getElementById("nowPlaying");
 
     // Regex for catching the "NP" thing, hopefully.
-    // var nowPlaying = radioAPIString.match("\"np\":\"*\",");
+
+    alert(radioAPIString);
+    var nowPlaying = radioAPIString.match("\"np\":\"*\",");
+    alert(nowPlaying);
+
     var placeholder = "Temporary Listening Placeholder";
 
     document.getElementById("nowPlaying").innerHTML = placeholder;
