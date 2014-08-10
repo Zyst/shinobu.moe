9cb2315d99fba058e15e562bab205bc2a86458c7
Erick Romero <zyst@gmx.com>
2014-08-10 05:14:46 -0500

    Removed now deprecated code and temporarily editted out some stuff to avoid failing all sanity checks

--- js/r-a-dio.js
+++ js/r-a-dio.js
@@ -1,20 +1,11 @@
 function loadAPI() {
+    
+    // var getPHPString = document.getElementById("hidePHP");
+    // var radioAPIString = getPHPString.textContent;
 
-    // Goes to r-a-d.io/api, gets content
-    // var radioAPIString = "<?php Print($curlResult); ?>";
-
-    // The Div that contains "Now Playing"
-    // Do I need this?
-    // var nowPlayingDiv = document.getElementById("nowPlaying");
-
-    // Regex for catching the "NP" thing, hopefully.
-
-    var getPHPString = document.getElementById("hidePHP");
-    var radioAPIString = getPHPString.textContent;
-
-    alert(radioAPIString);
-    var nowPlaying = radioAPIString.match("\"np\":\"*\",");
-    alert(nowPlaying);
+    // alert(radioAPIString);
+    // var nowPlaying = radioAPIString.match("\"np\":\"*\",");
+    // alert(nowPlaying);
 
     var placeholder = "Temporary Listening Placeholder";
 
