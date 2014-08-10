feac8341b625030e43ab34f87795e1765d7d1d42
Erick Romero <zyst@gmx.com>
2014-08-10 02:17:49 -0500

    Even more changes! Fuuuuck

--- index.php
+++ index.php
@@ -48,7 +48,15 @@
                 <!-- Now playing block -->
                 <div id = "nowPlaying">
                     <script>
-                        loadAPI();
+                        // Goes to r-a-d.io/api, gets content
+                        var radioAPIString = "<?php Print($curlResult); ?>";
+
+                        // The Div that contains "Now Playing"
+                        var nowPlayingDiv = document.getElementById("nowPlaying");
+                        // Regex for catching the "NP" thing, hopefully.
+                        var nowPlaying = radioAPIString.match("\"np\":\"*\",");
+
+                        document.getElementById("nowPlaying").innerHTML = nowPlaying;
                     </script>
                 </div>
             
