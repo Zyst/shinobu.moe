bd966e6a57213df2c1f845e388f7ac8785de8b6c
Erick Romero <zyst@gmx.com>
2014-08-10 23:17:40 -0500

    Significantly cleaned up the code. Regained 10 sanity.

--- js/r-a-dio.js
+++ js/r-a-dio.js
@@ -1,16 +1,12 @@
-// function encode_utf8( s ){
-//     return unescape( encodeURIComponent( s ) );
-// }
-
 function loadAPI() {
+    
     // This gets the PHP Code
     var getPHPString = document.getElementById("hidePHP");
     var radioAPIString = getPHPString.textContent;
-    
-    // radioAPIString = encode_utf8( radioAPIString );
 
     // Updates "Now Playing"
     var nowPlaying = radioAPIString.match("\"np\":\".*\"?,\"listeners\"");
+
     nowPlaying = String(nowPlaying);
     nowPlaying = nowPlaying.replace("\"np\":\"", "");
     nowPlaying = nowPlaying.replace("\",\"listeners\"", "");
@@ -30,7 +26,8 @@ function loadAPI() {
     DJ = DJ.replace(/.*\"/, "");
 
 
-    // These actually update the divs with the content
+    // These actually update the divs with the content parsed
+    // with the wizardry above.
     document.getElementById("nowPlaying").innerHTML = nowPlaying;
     document.getElementById("DJ").innerHTML = DJ;
 }
