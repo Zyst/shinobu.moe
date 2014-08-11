7b98a5b219343878bb65fb497961adffb8f11751
Erick Romero <zyst@gmx.com>
2014-08-10 22:30:23 -0500

    Test

--- js/r-a-dio.js
+++ js/r-a-dio.js
@@ -20,14 +20,15 @@ function loadAPI() {
 
 
     // Updates "DJ" (WIP)
-    var DJ = radioAPIString.match("\"djname\":\".*\"?,\"djtext\"");
+    // var DJ = radioAPIString.match("\"djname\":\".*\"?,\"djtext\"");
 
-    DJ = String(DJ);
-    DJ = DJ.replace("\"djname\":\"", "");
-    DJ = DJ.replace("\",\"djtext\"", "");
-    DJ = DJ.replace(/.*\"/, "");
-    DJ = String(DJ);
-    // alert(DJ);
+    // DJ = String(DJ);
+    // DJ = DJ.replace("\"djname\":\"", "");
+    // DJ = DJ.replace("\",\"djtext\"", "");
+    // DJ = DJ.replace(/.*\"/, "");
+    // DJ = String(DJ);
+    // // alert(DJ);
+    var DJ = "Neru feat. \u93e1\u97f3\u30ea\u30f3 - \u30a2\u30d6\u30b9\u30c8\u30e9\u30af\u30c8\u30fb\u30ca\u30f3\u30bb\u30f3\u30b9";
 
 
     // These actually update the divs with the content
