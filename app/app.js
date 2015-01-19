4a5e180b2126beac99241197377959653b330250
Zyst <zyst@gmx.com>
2015-01-19 14:57:32 -0600

    What happens if I do this?

--- app/app.js
+++ app/app.js
@@ -1,10 +1,21 @@
 angular.module("shinobuApp", [])
 
-    .controller("mainController", function() {
+    // Inject $http to our controller
+    .controller("mainController", function($http) {
 
         // Bind this to view model (vm)
         var vm = this;
 
         vm.helloWorld = "Success!";
+        
+        // Make the API call
+        $http.get("https://r-a-d.io/api")
+            .then(function(data) {
+                
+                // Bind the information we recieve
+                // to vm.radio
+                vm.radio = data;
+                
+            });
 
     });
