2f002d82fc4eaef4304bd3490519edb9305340c6
Zyst <zyst@gmx.com>
2015-01-19 14:43:36 -0600

    Beta.shinobu.moe start

---
+++ app/app.js
@@ -0,0 +1,10 @@
+angular.module("shinobuApp")
+
+    .controller("mainController", function() {
+
+        // Bind this to view model (vm)
+        var vm = this;
+
+        vm.helloWorld = "Success!";
+
+    });
