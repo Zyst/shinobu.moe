d089e8d5167b10214d5043f66ad172a08138effe
Zyst <zyst@gmx.com>
2015-01-19 18:22:33 -0600

    NP tesT

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ angular.module("shinobuApp", [])
                 // to vm.radio
                 vm.radio = data;
                 
-                vm.nowPlaying = data.np;
+                vm.nowPlaying = data.main.np;
                 
             });
 
