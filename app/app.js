90875d6e41b4325ca312bec5832b5aab08580123
Zyst <zyst@gmx.com>
2015-03-08 01:08:35 -0600

    testing alert

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ angular.module("shinobuApp", [])
         // Bind this to view model (vm)
         var vm = this;
         
-        // Make the API call
+        // Make the API call for the main information
         $http.get("https://r-a-d.io/api")
             .then(function(data) {
                 
@@ -17,11 +17,25 @@ angular.module("shinobuApp", [])
                 // Our commonly used JSON entries get bound
                 vm.nowPlaying  = vm.radio.main.np;
                 vm.dj          = vm.radio.main.dj.djname;
+                vm.djId        = vm.radio.main.dj.id;
                 vm.listeners   = vm.radio.main.listeners;
                 vm.currentTime = vm.radio.main.current;
                 vm.startTime   = vm.radio.main.start_time;
                 vm.endTime     = vm.radio.main.end_time;
-                
+
             });
-        
+
+        var djIdForCall = "https://r-a-d.io/api/dj-image/" + vm.dj;
+
+        alert(djIdForCall);
+
+        $http.get(djIdForCall)
+            .then(function(data) {
+
+                // Bind the information we receive
+                // to vm.djImage
+                vm.djImage = data;
+
+            });
+
     });
