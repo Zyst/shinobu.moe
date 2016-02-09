ba2dfb1562c2ef3430a015a23dc2bc822208fdac
Zyst <zyst@gmx.com>
2016-02-08 20:37:36 -0600

    Completely refactored everything, last.fm and more

--- app/app.js
+++ app/app.js
@@ -1,181 +1,251 @@
-angular.module("shinobuApp", [])
-
-    // Inject $http to our controller
-    .controller("mainController", function($http) {
-
-        // Bind this to view model (vm)
-        var vm = this;
+﻿angular.module("shinobuApp", [])
+    .controller('mainController', function ($http) {
         
-        vm.randomImage = Math.floor((Math.random() * (14)) + 1);
+        // Our view model
+        var vm = this;
 
-        callApi();
+        // Have we started? To show/hide our divs
+        vm.started = false;
 
-        function callApi() {
+        // What we'll use to contain our front end display
+        vm.display = {};
 
-            // Make sure the interval has been cleared
-            if (typeof countUp != "undefined") {
-                clearInterval(countUp);
-            }
+        // r/a/dio path
+        var callRadio = function () {
 
-            if (typeof concurrent != "undefined") {
-                clearInterval(concurrent);
-            }
+            vm.playingRadio = true;
 
-            // Make the API call for the main information
-            $http.get("https://r-a-d.io/api")
-                .then(function(data) {
+            vm.started = true;
 
-                    // Bind the information we receive
-                    // to vm.radio
-                    vm.radio = data.data;
-                    vm.equal = data.data;
+            $http({
+                method: 'GET',
+                url: 'https://r-a-d.io/api'
+            }).then(function successCallback(data) {
 
-                    // Our commonly used JSON entries get bound
-                    vm.nowPlaying  = vm.radio.main.np;
-                    vm.dj          = vm.radio.main.dj.djname;
-                    vm.djId        = vm.radio.main.dj.id;
-                    vm.listeners   = vm.radio.main.listeners;
-                    vm.currentTime = vm.radio.main.current;
-                    vm.startTime   = vm.radio.main.start_time;
-                    vm.endTime     = vm.radio.main.end_time;
+                vm.radio = data.data;
 
-                })
+                vm.display.playing = vm.radio.main.np;
 
-                .then(function(data) {
+                // Our commonly used JSON entries get bound
+                vm.display.playing = vm.radio.main.np;
+                vm.display.dj = vm.radio.main.dj.djname;
+                vm.display.image = 'https://r-a-d.io/api/dj-image/' + vm.radio.main.dj.id;
+                vm.display.listeners = vm.radio.main.listeners;
 
-                    // We bind the image link once we get a response, because
-                    // we can't bind this until we are sure we have the data
-                    vm.djImage = "https://r-a-d.io/api/dj-image/" + vm.djId;
+                // Object with our 'time' information
+                vm.time = {
+                    currentTime: vm.radio.main.current - vm.radio.main.start_time,
+                    endTime: vm.radio.main.end_time - vm.radio.main.start_time,
+                    endTimeMinutes: Math.floor((vm.radio.main.end_time - vm.radio.main.start_time) / 60),
+                    endTimeSeconds: zeroPadding((vm.radio.main.end_time - vm.radio.main.start_time) % 60)
+                }
 
-                    // We get the current time (Song position) in seconds
-                    vm.currentTime = vm.currentTime - vm.startTime;
+                // We call the increase time function
+                increaseTime();
+                checkRadioConcurrency();
 
-                    // Here we turn the numbers into Minutes and seconds
-                    vm.currentTimeMinutes = Math.floor(vm.currentTime / 60);
-                    vm.currentTimeSeconds = vm.currentTime % 60;
+            }, function errorCallback(err) {
 
-                    // We'll add a 0 pad
-                    vm.currentTimeSeconds = zeroPadding(vm.currentTimeSeconds);
+                console.log('Somebody messed up');
 
-                    // We get the end time (Song end) in seconds
-                    vm.endTime = vm.endTime - vm.startTime;
+            });
 
-                    vm.endTimeMinutes = Math.floor(vm.endTime / 60);
-                    vm.endTimeSeconds = vm.endTime % 60;
-
-                    // We add a 0 pad
-                    vm.endTimeSeconds = zeroPadding(vm.endTimeSeconds);
-
-                    document.getElementById("time").innerHTML = vm.currentTimeMinutes + ":" + vm.currentTimeSeconds +
-                    "/" + vm.endTimeMinutes     + ":" + vm.endTimeSeconds;
-
-                    // Added this so it updates on pageload because my OCD was killing me
-                    var playingPercentage = vm.currentTime * 100 / vm.endTime;
-                    document.getElementById("playingBar").style.width = playingPercentage + "%";
-
-                    // Starts the Timer
-                    var countUp = setInterval(increaseTime, 1000);
-
-                    // Checks for concurrency every 5 seconds
-                    var concurrent = setInterval(checkSongConcurrency, 5000);
-
-                    /**
-                     * * * * TIME AND PLAYING BAR HANDLER * * * *
-                     *
-                     * Increases time in 1 second increments
-                     * @return {void} Will just keep going until
-                     *                  it's equal to endtime.
-                     *
-                     * This also handles the playing bar now.
-                     *   It multiplies by laws of threes to get percentages,
-                     *     so it multiplies Current Time by 100 and divides it by Endtime.
-                     *
-                     */
-                    function increaseTime() {
-
-                        ++vm.currentTime;
-
-                        // Here we turn the numbers into Minutes and seconds
-                        vm.currentTimeMinutes = Math.floor(vm.currentTime / 60);
-
-                        vm.currentTimeSeconds = vm.currentTime % 60;
-
-                        // We'll add a 0 pad
-                        vm.currentTimeSeconds = zeroPadding(vm.currentTimeSeconds);
-
-                        document.getElementById("time").innerHTML = vm.currentTimeMinutes + ":" + vm.currentTimeSeconds +
-                        "/" + vm.endTimeMinutes     + ":" + vm.endTimeSeconds;
-
-                        /**
-                         * We are now going to do the playing bar width adjustments.
-                         *   Percentage based and laws of threes
-                         */
-                        if (vm.endTime > 0) {
-                            // If the 'endTime' of song is above 0 update as normal
-                            playingPercentage = vm.currentTime * 100 / vm.endTime;
-                        } else {
-                            // If the endTime is 0 which some DJs do pretty often
-                            //   it'll just show a full bar. This is mostly a stylistic choice.
-                            playingPercentage = 100;
-                        }
-
-                        document.getElementById("playingBar").style.width = playingPercentage + "%";
-
-                        // If time reaches end time this stops.
-                        //   It also makes sure endtime is not zero so it keeps going even if
-                        //     DJ didn't pass time metadata or for whatever reason that happens.
-                        //     otherwise it gets stuck after the first update when time is 0
-                        if ((vm.currentTime >= vm.endTime) && (vm.endTime > 0)) {
-                            clearInterval(countUp);
-
-                            checkSongConcurrency();
-                        }
-                    }
-
-                    /**
-                     * Adds a 0 if the seconds are in the 0X range
-                     * @param  {int} seconds    Initial Seconds input
-                     * @return {String}         First case: Same as input
-                     *                          Second case: 0+X with X = input
-                     */
-                    function zeroPadding(seconds) {
-
-                        seconds = String(seconds);
-
-                        if (seconds.length > 1) {
-                            // Basically do nothing
-                            return seconds;
-                        } else {
-                            return "0" + seconds;
-                        }
-                    }
-
-                    /**
-                     * Checks if the 'now playing' song has changed, if it has
-                     * it calls the api once more to update the data on the site
-                     * 
-                     * @return {void} [Doesn't return anything, just calls the api when it finds a difference and
-                     *                 clears the interval to avoid some Interval errors]
-                     */
-                    function checkSongConcurrency() {
-                        $http.get("https://r-a-d.io/api")
-                            .then(function(data) {
-
-                                // Bind the information we receive
-                                // to vm.equal to check if it's still the same
-                                vm.equal = data.data;
-
-                                // Might as well update listeners, doing nothing with the query would be a waste.
-                                vm.listeners = vm.equal.main.listeners;
-
-                                if (vm.equal.main.np != vm.nowPlaying) {
-                                    clearInterval(countUp);
-                                    clearInterval(concurrent);
-
-                                    callApi();
-                                }
-                            });
-                    }
-                });
         }
+
+        // Checks if we are still playing the same song
+        var checkRadioConcurrency = function () {
+
+            $http({
+                method: 'GET',
+                url: 'https://r-a-d.io/api'
+            }).then(function successCallback(data) {
+
+                vm.display.listeners = data.data.main.listeners;
+
+                if (data.data.main.np !== vm.display.playing) {
+                    
+                    clearTimeoutsAndRestart();
+
+                } else {
+
+                    vm.concurrencyTimeout = setTimeout(checkRadioConcurrency, 5000);
+                }
+            
+            }, function errorCallback(err) {
+                console.log('Somebody messed up');
+
+            });
+        }
+
+        // Clears timeouts and calls r/a/dio
+        var clearTimeoutsAndRestart = function () {
+
+            clearTimeout(vm.timeTimeout);
+            clearTimeout(vm.concurrencyTimeout);
+
+            callRadio();
+        }
+
+        // Increases seconds in our playing, also handles the playing bar
+        var increaseTime = function () {
+
+            vm.time.currentTime++;
+
+            // We turn time into Minutes and seconds
+            vm.time.currentTimeMinutes = Math.floor(vm.time.currentTime / 60);
+            vm.time.currentTimeSeconds = zeroPadding(vm.time.currentTime % 60);
+
+            document.getElementById("time").innerHTML = vm.time.currentTimeMinutes + ":" + vm.time.currentTimeSeconds +
+                        "/" + vm.time.endTimeMinutes + ":" + vm.time.endTimeSeconds;
+
+            // Play bar is 100% by default
+            var playingPercentage = 100;
+
+            if (vm.time.endTime > 0) {
+                playingPercentage = vm.time.currentTime * 100 / vm.time.endTime;
+            }
+
+            document.getElementById("playing-bar").style.width = playingPercentage + "%";
+
+            if (vm.time.currentTime >= vm.time.endTime && vm.time.endTime > 0) {
+
+                clearTimeout(vm.timeTimeout);
+            } else {
+
+                vm.timeTimeout = setTimeout(increaseTime, 1000);
+            }
+
+        }
+
+        // Last.fm path
+        var callLastfm = function () {
+
+            vm.playingLastFm = true;
+
+            $http({
+                method: 'GET',
+                url: 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=Zystx&api_key=5b801a66d1a34e73b6e563afc27ef06b&limit=1&format=json'
+            }).then(function successCallback(data) {
+
+                vm.lastfm = data.data.recenttracks;
+
+                // We save the currenly playing song and artist 
+                vm.display.playing = vm.lastfm.track[0].artist['#text'] + ' - ' + vm.lastfm.track[0].name;
+                
+                // A default image in case there's no artwork in our Last.fm
+                vm.display.image = 'images/dj.jpg';
+                
+                // We grab the largest image that exists
+                vm.lastfm.track[0].image.forEach(function (image) {
+                    
+                    // If the image is different from 'empty' this is our new image
+                    if (image['#text'] !== '') {
+                        vm.display.image = image['#text'];
+                    }
+
+                }, this);
+                
+                // DJ is hardcoded for Last.fm
+                vm.display.dj = 'Zyst';
+                
+                // We have completed a cycle
+                vm.started = true;
+
+                setTimeout(callLastfm, 5000);
+
+            }, function errorCallback(err) {
+
+                console.log('Somebody messed up');
+
+            });
+
+        }
+
+        // We add a 0 to numbers below 10 ie: 09, 03
+        var zeroPadding = function (seconds) {
+
+            seconds = String(seconds);
+
+            if (seconds.length > 1) {
+
+                // Basically do nothing
+                return seconds;
+
+            } else {
+
+                return "0" + seconds;
+            }
+        }
+
+        // Determines which image is our Background randomly
+        var randomBG = function () {
+
+            var bg = document.body;
+            var randomnumber = Math.floor((Math.random() * (14)) + 1);
+
+            bg.style.backgroundImage = "url('images/" + randomnumber + ".png')";
+        }
+
+        // Start getting dataa
+        var start = function () {
+
+            // Random background
+            randomBG();
+
+            $http({
+                method: 'GET',
+                // Replace user=Zystx here with your Lastfm user name
+                url: 'http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=Zystx&api_key=5b801a66d1a34e73b6e563afc27ef06b&limit=1&format=json'
+            }).then(function successCallback(lastfm) {
+                
+                // We bind the Last.fm data
+                vm.lastfm = lastfm.data.recenttracks;
+
+                $http({
+                    method: 'GET',
+                    url: 'https://r-a-d.io/api'
+                }).then(function successCallback(radio) {
+                    
+                    // We bind the r/a/dio data
+                    vm.radio = radio.data;
+                    
+                    // We create variables for comparison
+                    var radioPlaying = vm.radio.main.np;
+                    var lastFmPlaying = vm.lastfm.track[0].artist['#text'] + ' - ' + vm.lastfm.track[0].name;
+
+                    var notUndefined = vm.lastfm.track[0] !== undefined &&
+                        vm.lastfm.track[0]['@attr'] !== undefined &&
+                        vm.lastfm.track[0]['@attr'].nowplaying !== undefined;
+
+                    // If you are playing the same thing as r/a/dio
+                    if ((radioPlaying.toUpperCase() === lastFmPlaying.toUpperCase()) &&
+                        notUndefined && vm.lastfm.track[0]['@attr'].nowplaying === "true") {
+
+                        callRadio();
+
+                    // You are playing something in Last.fm (Different from r/a/dio)
+                    } else if (notUndefined && vm.lastfm.track[0]['@attr'].nowplaying === "true") {
+
+                        callLastfm();
+
+                        // We aren't playing anything so we show r/a/dio
+                    } else {
+
+                        callRadio();
+                    }
+
+                }, function errorCallback(err) {
+
+                    // We use Last.fm
+                    callLastfm();
+                });
+
+            }, function errorCallback(err) {
+
+                // Last fm is down so we do r/a/dio
+                callRadio();
+            });
+        }
+
+        start();
     });
