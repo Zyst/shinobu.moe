de0e1e578945570e356b3aebd1197cc09996a273
Erick Romero <erick.romero@aeris.net>
2018-01-02 15:53:37 -0600

    Progress update

--- app/app-vanilla.js
+++ app/app-vanilla.js
@@ -1,72 +1,115 @@
-const LAST_FM_URL =
-  'https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=Zystx&api_key=57ee3318536b23ee81d6b27e36997cde&limit=1&format=json'
-const RADIO_URL = 'https://r-a-d.io/api'
+// eslint-disable-next-line no-extra-semi
+;(() => {
+  const LAST_FM_URL =
+    'https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=Zystx&api_key=57ee3318536b23ee81d6b27e36997cde&limit=1&format=json'
+  const RADIO_URL = 'https://r-a-d.io/api'
 
-// Determines which image is our Background randomly
-const randomBG = () => {
-  const NUMBER_OF_IMAGES = 14
-  const MAKE_IT_NOT_ZERO = 1
+  const zeroPadding = seconds => {
+    var secondsString = String(seconds)
 
-  const randomNumber = Math.floor(
-    Math.random() * NUMBER_OF_IMAGES + MAKE_IT_NOT_ZERO
-  )
+    if (secondsString.length > 1) {
+      // Do nothing
+      return secondsString
+    }
 
-  const bg = document.body
+    return '0' + secondsString
+  }
 
-  bg.style.backgroundImage = `url('images/${randomNumber}.png')`
-}
+  const fetchandPass = (url, render) =>
+    fetch(url)
+      .then(response => response.json())
+      .then(response => render(response))
 
-const start = () => {
-  randomBG()
+  // TODO: Implement this
+  const renderRadio = radio => {
+    return radio
+  }
 
-  const promiseArray = [fetch(LAST_FM_URL), fetch(RADIO_URL)]
+  // TODO: Implement this
+  const renderLastFm = lastFm => {
+    return lastFm
+  }
 
-  /**
-   * We don't want to give up if one fails, r/a/dio or last.fm might be
-   * down, but not the other
-   */
-  Promise.all(promiseArray.map(p => p.catch(e => e)))
-    .then(responses => {
-      return Promise.all(
-        responses.map(r => {
-          if (!r.json) return r
+  // Determines which image is our Background randomly
+  const randomBG = () => {
+    const NUMBER_OF_IMAGES = 14
+    const MAKE_IT_NOT_ZERO = 1
 
-          return r.json()
-        })
-      )
-    })
-    .then(responses => {
-      const [lastFm, radio] = responses
+    const randomNumber = Math.floor(
+      Math.random() * NUMBER_OF_IMAGES + MAKE_IT_NOT_ZERO
+    )
 
-      // If everything went wrong, we retry in 5 seconds
-      if (lastFm instanceof Error && radio instanceof Error) {
-        const RETRY_IN_MILISECONDS = 5000
+    const bg = document.body
 
-        setTimeout(start, RETRY_IN_MILISECONDS)
-      }
+    bg.style.backgroundImage = `url('images/${randomNumber}.png')`
+  }
 
-      // If Last.fm is an error, and r/a/dio isn't
-      if (lastFm instanceof Error && !(radio instanceof Error)) {
-        // TODO: Render r/a/dio
-      }
+  const start = () => {
+    randomBG()
 
-      // If radio is an error, and last.fm isn't
-      if (radio instanceof Error && !(lastFm instanceof Error)) {
-        // TODO: Render last.fm
-      }
+    const promiseArray = [fetch(LAST_FM_URL), fetch(RADIO_URL)]
 
-      const CURRENT_SONG_INDEX = 0
+    /**
+     * We don't want to give up if one fails, r/a/dio or last.fm might be
+     * down, but not the other
+     */
+    Promise.all(promiseArray.map(p => p.catch(e => e)))
+      .then(responses => {
+        return Promise.all(
+          responses.map(r => {
+            if (!r.json) return r
 
-      // If we got here, that means both calls finished successfully
-      // TODO: If r/a/dio and last.fm are playing the same thing, show r/a/dio
-      // otherwise, show last.fm
-      const radioPlaying = radio.main.np
-      const lastFmPlaying = `${
-        lastFm.recenttracks.track[CURRENT_SONG_INDEX].artist['#text']
-      } - ${lastFm.recenttracks.track[CURRENT_SONG_INDEX].name}`
+            return r.json()
+          })
+        )
+      })
+      .then(responses => {
+        const [lastFm, radio] = responses
 
-      console.log(radioPlaying, lastFmPlaying)
-    })
-}
+        // If everything went wrong, we retry in 5 seconds
+        if (lastFm instanceof Error && radio instanceof Error) {
+          const RETRY_IN_MILISECONDS = 5000
 
-start()
+          setTimeout(start, RETRY_IN_MILISECONDS)
+        }
+
+        // If Last.fm is an error, and r/a/dio isn't
+        if (lastFm instanceof Error && !(radio instanceof Error)) {
+          return renderRadio(radio)
+        }
+
+        const CURRENT_SONG_INDEX = 0
+        const lastFmSong = lastFm.recenttracks.track[CURRENT_SONG_INDEX]
+
+        // If radio is an error, and last.fm isn't
+        if (radio instanceof Error && !(lastFm instanceof Error)) {
+          return renderLastFm(lastFm)
+        }
+
+        // If we got here, that means both calls finished successfully
+        const radioPlaying = radio.main.np
+        const lastFmPlaying = `${lastFmSong.artist['#text']} - ${
+          lastFmSong.name
+        }`
+
+        const currentlyPlaying =
+          lastFmSong !== undefined &&
+          lastFmSong['@attr'] !== undefined &&
+          lastFmSong['@attr'].nowplaying !== undefined &&
+          lastFmSong['@attr'].nowplaying === 'true'
+
+        if (
+          currentlyPlaying &&
+          radioPlaying.toUpperCase() === lastFmPlaying.toUpperCase()
+        ) {
+          return renderRadio(radio)
+        } else if (currentlyPlaying) {
+          return renderLastFm(lastFm)
+        } else {
+          return renderRadio(radio)
+        }
+      })
+  }
+
+  start()
+})()
